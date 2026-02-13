c894d9747ed650d0df4b5d8ae69d79a290323786
LuisSambrano_UX <luissambranoarg@gmail.com>
2026-02-13 06:34:00 -0300

    fix: handle invalid dates in NewsCard and fix linting in test setup

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -5,8 +5,7 @@ import React from 'react';
 // Mock next/image
 vi.mock('next/image', () => ({
   __esModule: true,
-  default: (props: any) => {
-    // eslint-disable-next-line @next/next/no-img-element
+  default: (props: React.ImgHTMLAttributes<HTMLImageElement> & { fill?: boolean }) => {
     return React.createElement('img', { ...props, fill: props.fill ? "true" : undefined });
   },
 }));
@@ -14,13 +13,13 @@ vi.mock('next/image', () => ({
 // Mock framer-motion
 vi.mock('framer-motion', () => ({
   motion: {
-    article: ({ children, ...props }: any) => React.createElement('article', props, children),
-    div: ({ children, ...props }: any) => React.createElement('div', props, children),
-    h3: ({ children, ...props }: any) => React.createElement('h3', props, children),
-    p: ({ children, ...props }: any) => React.createElement('p', props, children),
-    span: ({ children, ...props }: any) => React.createElement('span', props, children),
+    article: ({ children, ...props }: { children: React.ReactNode } & React.HTMLAttributes<HTMLElement>) => React.createElement('article', props, children),
+    div: ({ children, ...props }: { children: React.ReactNode } & React.HTMLAttributes<HTMLElement>) => React.createElement('div', props, children),
+    h3: ({ children, ...props }: { children: React.ReactNode } & React.HTMLAttributes<HTMLElement>) => React.createElement('h3', props, children),
+    p: ({ children, ...props }: { children: React.ReactNode } & React.HTMLAttributes<HTMLElement>) => React.createElement('p', props, children),
+    span: ({ children, ...props }: { children: React.ReactNode } & React.HTMLAttributes<HTMLElement>) => React.createElement('span', props, children),
   },
-  AnimatePresence: ({ children }: any) => children,
+  AnimatePresence: ({ children }: { children: React.ReactNode }) => children,
 }));
 
 // Mock lucide-react
